0acd9d51820f148f633e64e01db02c83ba67f582
NITIN <galla.nitin2003@gmail.com>
2024-01-14 14:18:57 +0530

    development work

---
+++ server/src/main/java/com/invoice/server/services/InvoiceService.java
@@ -0,0 +1,16 @@
+package com.invoice.server.services;
+
+import com.invoice.server.model.Invoice;
+
+import java.util.List;
+
+public interface InvoiceService {
+
+    public Invoice addInvoice(Invoice invoice);
+
+
+    public List<Invoice> getInvoices();
+
+
+    public Invoice deleteInvoice(long id);
+}
